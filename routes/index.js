1aab55857a97b9101bf7f7b00b8082464f28c028
snehasahu7 <btech15146.22@bitmesra.ac.in>
2024-07-11 23:28:51 +0530

    init

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/alia', function(req, res, next) {
+  res.render('alia', { title: 'alia' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'login' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'register' });
+});
+router.get('/taylor', function(req, res, next) {
+  res.render('taylor', { title: 'taylor' });
+});
+router.get('/store', function(req, res, next) {
+  res.render('store', { title: 'store' });
+});
+
+module.exports = router;
