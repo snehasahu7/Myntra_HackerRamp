b3fe66cad0c8be5913ef83213a4559a759e80d82
snehasahu7 <btech15146.22@bitmesra.ac.in>
2024-07-14 15:27:00 +0530

    routes modified

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/alia', function(req, res, next) {
-  res.render('alia', { title: 'alia' });
-});
+
 router.get('/login', function(req, res, next) {
   res.render('index', { title: 'login' });
 });
-
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'register' });
-});
-router.get('/taylor', function(req, res, next) {
-  res.render('taylor', { title: 'taylor' });
 });
 router.get('/store', function(req, res, next) {
   res.render('store', { title: 'store' });
@@ -25,6 +19,10 @@ router.get('/shop', function(req, res, next) {
 router.get('/india', function(req, res, next) {
   res.render('indianStore', { title: 'indianStore' });
 });
-
-
+router.get('/alia', function(req, res, next) {
+  res.render('alia', { title: 'alia' });
+});
+router.get('/taylor', function(req, res, next) {
+  res.render('taylor', { title: 'taylor' });
+});
 module.exports = router;
