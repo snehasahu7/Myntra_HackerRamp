a63f851cb0a78de5b5377e8a9fc57008857e3551
snehasahu7 <btech15146.22@bitmesra.ac.in>
2024-07-15 23:34:22 +0530

    modified

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/api/goals", require("./routes/goalRoutes"));
+
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
