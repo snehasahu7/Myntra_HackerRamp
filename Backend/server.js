703e8ae7078188da3d4d8d90fcbae786ac68fb4f
snehasahu7 <btech15146.22@bitmesra.ac.in>
2024-07-15 16:47:21 +0530

    modification

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const NODE_ENV = process.eventNames.NODE_ENV || "production";
 
@@ -13,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
